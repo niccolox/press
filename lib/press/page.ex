d3412830962a721d61b24749778ed472ee80f4fa
c4lliope <code@assembled.app>
2023-08-15 11:37:00 -0400

    Clean up code in `lib/press/page.ex`

--- lib/press/page.ex
+++ lib/press/page.ex
@@ -3,10 +3,15 @@ defmodule Press.Page do
   defstruct [:key, :composer, :name, :body, :summary, :labels, :day]
 
   def build(node, angles, body) do
-    # import IEx; IEx.pry
-    [node_name] = node |> Path.rootname |> Path.split |> Enum.take(-1)
-    [clock, key] = node_name |> String.split(".", parts: 2)
-    day = Date.from_iso8601!(clock)
-    struct!(__MODULE__, [key: key, day: day, body: body] ++ Map.to_list(angles))
+    [clock, key] = (
+      node
+      |> Path.rootname |> Path.split |> List.last
+      |> String.split(".", parts: 2)
+    )
+    struct!(__MODULE__, [
+      key: key,
+      day: Date.from_iso8601!(clock),
+      body: body,
+    ] ++ Map.to_list(angles))
   end
 end
