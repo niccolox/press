e47c265d9f49072e1dfd9c40b90c9291f1824eb9
c4lliope <code@assembled.app>
2023-11-13 14:45:31 -0500

    Use `.sface` template in `IndexLive`.

--- lib/assembled_web/live/index_live.ex
+++ lib/assembled_web/live/index_live.ex
@@ -7,32 +7,4 @@ defmodule AssembledWeb.IndexLive do
     idx = Assembled.Rails.index("hconres", 110) |> Enum.map(fn {_,m} -> m end)
     { :ok, socket |> assign(:index, idx) }
   end
-
-  def render(assigns) do
-    ~F"""
-    <style>
-      .measure {
-        margin-bottom: 2rem;
-        border: 4px solid #2d2d2d;
-        border-radius: 8px;
-        padding: 2rem;
-        background: #c09999;
-        color: #062743;
-      }
-    </style>
-
-    <h1><a href="/home">Assembled Index</a></h1>
-
-    {#for measure <- @index}
-      <div class="measure">
-        <h2>
-          {measure["angles"]["index"]["congress"]}
-          {measure["angles"]["index"]["type"]}
-          {measure["angles"]["index"]["number"]}
-        </h2>
-        <code>{measure |> Poison.encode!}</code>
-      </div>
-    {/for}
-    """
-  end
 end
